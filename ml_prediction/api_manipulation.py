2432e98bec9e1bed6e0f84146b85763241ccb21b
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2024-08-21 20:56:47 -0500

    first commit

---
+++ ml_prediction/api_manipulation.py
@@ -0,0 +1,9 @@
+#TODO: will take the data from the api and manipulate it to be used in the model
+import requests
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=IBM&apikey=demo'
+r = requests.get(url)
+data = r.json()
+
+print(data)
---
+++ ml_prediction/model/stock_data_model.py
@@ -0,0 +1,5 @@
+class stock_data_model{
+    stock = None
+    def __init__(self, data):
+        self.data = data
+}
