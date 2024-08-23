9549232e5b872e9920bf084e0f5b4fbd90e7571a
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2024-08-22 21:41:08 -0500

    testing yfinance

--- ml_prediction/api_manipulation.py
+++ ml_prediction/api_manipulation.py
@@ -1,5 +1,9 @@
 import yfinance as yf
 #TODO: will take the data from the api and manipulate it to be used in the model
 
-test_data = yf.download("MSFT", start="2024-02-01", end="2024-04-01")
-print(test_data)
+test_data = yf.download("NFLX", period="5d")
+# print(test_data)
+
+stock = yf.Tickers("msft nflx aapl")
+
+print(stock.tickers["MSFT"].)
